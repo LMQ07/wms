9bd25df6555b90a42b3c2e5e3c16efdb2f4e6ad7
summer-MQ <mq1009416078@163.com>
2022-09-07 20:16:49 +0800

    已完成

--- src/main.js
+++ src/main.js
@@ -32,16 +32,16 @@ Vue.use(ElementUI)
 Vue.config.productionTip = false
 // import GetRoute from './layout/mixin/GetRoute'
 // Vue.mixin(GetRoute)
-router.beforeEach((to,from,next)=>{
+router.beforeEach((to, from, next) => {
   const navArr = store.getters.navArr
-  const flag = navArr.some((item) => item.path == to.path);
+  const flag = navArr.some((item) => item.path == to.path)
   if (!flag) {
-        // this.navArr.push(this.$route.meta.title);
-        store.commit("app/ADD_NAVBARITEM", {
-          title: to.meta.title,
-          path: to.path,
-        });
-        store.commit('app/CHANGE_CURRENTINDEX',navArr.length-1)
+    // this.navArr.push(this.$route.meta.title);
+    store.commit('app/ADD_NAVBARITEM', {
+      title: to.meta.title,
+      path: to.path
+    })
+    store.commit('app/CHANGE_CURRENTINDEX', navArr.length - 1)
   }
   next()
 })
